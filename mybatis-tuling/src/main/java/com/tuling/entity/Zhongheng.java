db5ee359895c11e2699d0f4b070a50f131b36e16
jipeng <jipeng@able-elec.com>
2021-07-07 19:52:34 +0800

    初始化

---
+++ mybatis-tuling/src/main/java/com/tuling/entity/Zhongheng.java
@@ -0,0 +1,10 @@
+package com.tuling.entity;
+
+/**
+ * @param
+ * @author jipeng
+ * @date 2021-07-06 15:22
+ */
+public class Zhongheng {
+}
+
---
+++ mybatis-tuling/src/main/java/com/tuling/mapper/ZhonghengMapper.java
@@ -0,0 +1,10 @@
+package com.tuling.mapper;
+
+/**
+ * @author jipeng
+ * @date 2021-07-06 15:23
+ * @description
+ */
+public interface ZhonghengMapper {
+
+}
